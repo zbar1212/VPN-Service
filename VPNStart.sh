0cb5ccb839531f0dfa900eba66f827f76476a93d
zbar1212 <70152081+zbar1212@users.noreply.github.com>
2022-03-23 09:19:32 -0400

    Start OpenVPN

---
+++ VPNStart.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#Start VPN Connection (TryHackMe)
+
+#Deploy the bash script from the CARTP-Tool-Repository
+
+#Install OpenVPN
+sudo apt-get install -y openvpn
+sudo /etc/init.d/postgresql start
+sudo openvpn rjpark123.ovpn
+
+#Kill OpenVPN
+#sudo killall openvpn (type command if you want to completely kill OpenVpn)
+
+#Stop OpenVPN
+#Ctrl+C (this is to stop the OpenVPN process)
+
+#run OpenVPN on one powershell window
+#run CARTP on one powershell window
+#In total you will be operating with two powershell windows
